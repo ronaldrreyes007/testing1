0d265013b91e5473a01fce8236c8699876fa34b0
ronaldrreyes007 <RRR940605vvv>
2023-05-31 16:39:16 -0500

    Utilizar mojito para simular el lanzamiento del dado

---
+++ src/test/java/com/platzi/javatests/util/player/PlayerTest.java
@@ -0,0 +1,30 @@
+package com.platzi.javatests.util.player;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void looses_when_dice_number_is_too_low() {
+
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+
+        Player player = new Player(dice, 3);
+        assertFalse(player.play());
+    }
+
+    @Test
+    public void wins_when_dice_number_is_big() {
+
+        Dice dice = Mockito.mock(Dice.class);
+        //Esta linea simula el dado esta forzando el dado para que  retorne el 4
+        Mockito.when(dice.roll()).thenReturn(4);
+
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
+}
