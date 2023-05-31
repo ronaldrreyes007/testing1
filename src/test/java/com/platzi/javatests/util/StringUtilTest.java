3422134ddc0cc5ad96e7d8cf65242d2bc19c0a5a
ronaldrreyes007 <RRR940605vvv>
2023-05-31 08:39:18 -0500

    Varios test en una misma clase

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -3,15 +3,28 @@ package com.platzi.javatests.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.PrivilegedActionException;
+
 public class StringUtilTest {
     //cUANDO HACEMOS TEST NO ES NECESARIO EL MAIN
     //public static void main(String[] args) {
     @Test
-    public void testRepeat(){
-
-        Assert.assertEquals("holaholahola",StringUtil.repeat("hola", 3) );
+    public void testRepeatStringOnce(){
         Assert.assertEquals("hola",StringUtil.repeat("hola", 1) );
     }
+    @Test
+    public void testRepeatStringMultipleTimes(){
+        Assert.assertEquals("holaholahola",StringUtil.repeat("hola", 3) );
+    }
+    @Test
+    public void testRepeatStringZeroTimes(){
+        Assert.assertEquals("",StringUtil.repeat("hola", 0) );
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeatStringNegativeTimes(){
+      StringUtil.repeat("hola", -1) ;
+    }
+
     //Borramos por que ya vienen incluidas en las librerias de Junit
     /*private static void assertEquals(String actual, String expected) {
 
