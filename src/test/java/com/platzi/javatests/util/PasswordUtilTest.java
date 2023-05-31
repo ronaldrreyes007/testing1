6ce87811d213d251e169d4612313a9b985c64e44
ronaldrreyes007 <RRR940605vvv>
2023-05-31 16:12:08 -0500

    tESTING PARA CONPROBACION DE CONTRASEÑAS

---
+++ src/test/java/com/platzi/javatests/util/PasswordUtilTest.java
@@ -0,0 +1,26 @@
+package com.platzi.javatests.util;
+
+import org.junit.Test;
+
+import static com.platzi.javatests.util.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_letters() {
+       assertEquals(WEAK, PasswordUtil.assessPassword("12300!"));
+    }
+    @Test
+    public void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdefghk"));
+    }
+    @Test
+    public void medium_when_has_letters_and_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("abcd1234"));
+    }
+    @Test
+    public void strong_when_has_letters_and_numbers_and_simbols() {
+        assertEquals(STRONG, PasswordUtil.assessPassword("abcd123!"));
+    }
+}
